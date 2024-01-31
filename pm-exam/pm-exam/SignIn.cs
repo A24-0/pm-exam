6728b3660687d0ca463ba1c56d1d7718160023e3
A24 <desu99@bk.ru>
2024-01-31 20:49:57 +0300

    fc

---
+++ pm-exam/pm-exam/SignIn.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace pm_exam
+{
+    class SignIn : INotifyPropertyChanged
+    {
+        private string username;
+        public string Username
+        {
+            get { return username; }
+            set
+            {
+                if (username != value)
+                {
+                    username = value;
+                    OnPropertyChanged(nameof(Username));
+                }
+            }
+        }
+
+        private string email;
+        public string Email
+        {
+            get { return email; }
+            set
+            {
+                if (email != value)
+                {
+                    email = value;
+                    OnPropertyChanged(nameof(Email));
+                }
+            }
+        }
+
+        private string phoneNumber;
+        public string PhoneNumber
+        {
+            get { return phoneNumber; }
+            set
+            {
+                if (phoneNumber != value)
+                {
+                    phoneNumber = value;
+                    OnPropertyChanged(nameof(PhoneNumber));
+                }
+            }
+        }
+
+        private string password;
+        public string Password
+        {
+            get { return password; }
+            set
+            {
+                if (password != value)
+                {
+                    password = value;
+                    OnPropertyChanged(nameof(Password));
+                }
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ pm-exam/pm-exam/SignInOrRegistrationViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pm_exam
+{
+    class SignInOrRegistrationViewModel : ObservableObject
+    {
+        public RelayCommand SignInViewCommand { get; set; }
+        public RelayCommand RegistrationViewCommand { get; set; }
+
+        public SignIn SignIn_ { get; set; }
+        public Registration Registration_ { get; set; }
+
+        private object _currentView;
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public SignInOrRegistrationViewModel()
+        {
+            SignIn_ = new SignIn();
+            Registration_ = new Registration();
+
+            CurrentView = SignIn_;
+
+            SignInViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = SignIn_;
+            });
+
+            RegistrationViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = Registration_;
+            });
+        }
+    }
+}
